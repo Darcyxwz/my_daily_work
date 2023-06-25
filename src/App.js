41f0c692a2d552631c39df778c5a2fcbb900a26a
XIAO Weizhao <114809278+Darcyxwz@users.noreply.github.com>
2023-06-25 17:28:26 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Square({ value }) {
+  return <button className = "square">{value}</button>
+}
+
+export default function Board() {
+  return (
+    <div>
+      <div className = "board-row">
+        <Square value = "1" /><Square /><Square />
+      </div>
+      <div className = "board-row">
+        <Square /><Square /><Square />
+      </div>
+      <div className = "board-row">
+        <Square /><Square /><Square />
+      </div>
+    </div>
+  );
+  
+}
